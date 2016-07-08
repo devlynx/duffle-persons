6a86aad3542faee03d52d49de247b196c270a207
MWR <mreith@tetzel.com>
2016-07-08 11:03:10 -0600

    Initial Checkin

---
+++ duffle_persons/Controllers/OwnersController.cs
@@ -0,0 +1,73 @@
+﻿#region Copyright (c) 2016 Periwinkle Software Limited
+// MIT License -- https://opensource.org/licenses/MIT
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+// associated documentation files (the "Software"), to deal in the Software without restriction, 
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or 
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+namespace duffle_persons.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Net;
+    using Microsoft.AspNetCore.Mvc;
+    using Models;
+    using Swashbuckle.SwaggerGen.Annotations;
+
+    [Route("api/[controller]")]
+    public class OwnersController : Controller
+    {
+        // GET api/owners/5
+        [HttpGet("ping")]
+        public string Ping()
+        {
+            return "True";
+        }
+
+        // GET api/owners
+        [HttpGet]
+        [SwaggerResponse(HttpStatusCode.OK, Type = typeof(IEnumerable<Owner>))]
+        [SwaggerResponse(HttpStatusCode.BadRequest, Type = typeof(IEnumerable<Owner>))]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "Mike Reith", "M.S.I. Morton" };
+        }
+
+        // GET api/owners/5
+        [HttpGet("{id}")]
+        public string Get(Guid id)
+        {
+            return "value";
+        }
+
+        // POST api/owners
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/owners/5
+        [HttpPut("{id}")]
+        public void Put(Guid id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/owners/5
+        [HttpDelete("{id}")]
+        public void Delete(Guid id)
+        {
+        }
+    }
+}
---
+++ duffle_persons/Startup.cs
@@ -0,0 +1,111 @@
+﻿#region Copyright (c) 2016 Periwinkle Software Limited
+// MIT License -- https://opensource.org/licenses/MIT
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and 
+// associated documentation files (the "Software"), to deal in the Software without restriction, 
+// including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+// sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or 
+// substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING 
+// BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND 
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
+// DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#endregion
+
+namespace duffle_persons
+{
+    using System.IO;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Formatters;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.PlatformAbstractions;
+    using Newtonsoft.Json.Serialization;
+    using Swashbuckle.Swagger.Model;
+    using Serilog;
+    using Serilog.Sinks.RollingFile;
+    using Swashbuckle.SwaggerGen.Generator;
+
+    public class Startup
+    {
+        private IHostingEnvironment env { get; set; }
+        public Startup(IHostingEnvironment env)
+        {
+            this.env = env;
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsEnvironment("Development"))
+            {
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+
+            //services.AddTransient<IPersonsRepository, PersonsRepository>();
+            //services.AddTransient<IPersonsProcessor, PersonsProcessor>();
+
+            services.AddMvc();
+            services.AddSwaggerGen(c =>
+             {
+                 c.SingleApiVersion(new Info
+                 {
+                     Version = "v1",
+                     Title = "DUFFLE People API",
+                     Description = "This API manages DUFFLE people, both DRT personnel as well as animal owners/agents.",
+                     //TermsOfService = "Some terms ..."
+                 });
+                 //c.OperationFilter<AssignOperationVendorExtensions>();
+             });
+
+            services.ConfigureSwaggerGen(c => { c.IncludeXmlComments(GetXmlCommentsPath()); });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseCors(option => option.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
+            app.UseStaticFiles();
+
+            app.UseSwagger((httpRequest, swaggerDoc) =>
+             {
+                 swaggerDoc.Host = httpRequest.Host.Value;
+             });
+
+            app.UseSwaggerUi();
+
+            app.UseMvc();
+            Log.Logger = new LoggerConfiguration().WriteTo.RollingFile(@"Log-{Date}.txt").CreateLogger();
+        }
+        private static string GetXmlCommentsPath()
+        {
+            var app = PlatformServices.Default.Application;
+            return Path.Combine(app.ApplicationBasePath, app.ApplicationName + ".xml");
+        }
+    }
+}
